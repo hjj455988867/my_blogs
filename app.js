efa5b85b863a5ffdabfe0cd12215a2d3df325ad0
黄俊杰 <455988867@qq.com>
2021-05-11 17:09:09 +0800

    8

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-if ('serviceWorker' in navigator) {
+  if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/my_blogs/sw.js', { scope: '/my_blogs/' }).then(function(reg) {
-      // registration worked
-      console.log('Registration succeeded. Scope is ' + reg.scope);
+  
+      if(reg.installing) {
+        console.log('Service worker installing');
+      } else if(reg.waiting) {
+        console.log('Service worker installed');
+      } else if(reg.active) {
+        console.log('Service worker active');
+      }
+  
     }).catch(function(error) {
       // registration failed
       console.log('Registration failed with ' + error);
--- sw.js
+++ sw.js
@@ -2,9 +2,9 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
       caches.open('v1').then(function(cache) {
         return cache.addAll([
-          '/sw-test/',
-          '/sw-test/index.html',
-          '/sw-test/app.js'
+          '/my_blogs/',
+          '/my_blogs/index.html',
+          '/my_blogs/app.js'
         ]);
       })
     );
