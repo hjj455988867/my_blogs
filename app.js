50e76e1d74e5764772053d11d0551818208763de
黄俊杰 <455988867@qq.com>
2021-05-11 17:02:04 +0800

    7

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw-test/sw.js', { scope: '/sw-test/' }).then(function(reg) {
-  
-      if(reg.installing) {
-        console.log('Service worker installing');
-      } else if(reg.waiting) {
-        console.log('Service worker installed');
-      } else if(reg.active) {
-        console.log('Service worker active');
-      }
-  
+    navigator.serviceWorker.register('/my_blogs/sw.js', { scope: '/my_blogs/' }).then(function(reg) {
+      // registration worked
+      console.log('Registration succeeded. Scope is ' + reg.scope);
     }).catch(function(error) {
       // registration failed
       console.log('Registration failed with ' + error);
