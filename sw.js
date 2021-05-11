a5d770259cda9002a3a5af3f4b13a51f7e24ed0d
黄俊杰 <455988867@qq.com>
2021-05-11 17:44:29 +0800

    12

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', function(event) {
     event.waitUntil(
-      caches.open('v1').then(function(cache) {
+      caches.open('v2').then(function(cache) {
         return cache.addAll([
           '/my_blogs/',
           '/my_blogs/index.html',
