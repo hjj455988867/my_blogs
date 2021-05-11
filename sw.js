a9edc1fd600bf5c69962f21ef64eedcaab6c2b70
黄俊杰 <455988867@qq.com>
2021-05-11 17:51:16 +0800

    12

--- sw.js
+++ sw.js
@@ -39,3 +39,16 @@ self.addEventListener('install', function(event) {
       }
     }));
   });
+
+  self.addEventListener('activate', function(event) {
+    var cacheWhitelist = ['v2'];
+    event.waitUntil(
+      caches.keys().then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (cacheWhitelist.indexOf(key) === -1) {
+            return caches.delete(key);
+          }
+        }));
+      })
+    );
+  });
